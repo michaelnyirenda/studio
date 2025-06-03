79d707f4cc583256c2d8d004eb732a3fd5ef5271
Michael Nyirenda <michael00nyirenda@gmail.com>
2025-06-03 03:46:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
    And change it as follows:
    
    add an image that shows a health worker doing a screening. it should be in a cartoonish style

--- src/app/hiv-screening/page.tsx
+++ src/app/hiv-screening/page.tsx
@@ -1,5 +1,6 @@
 import ScreeningForm from '@/components/hiv-screening/screening-form';
 import PageHeader from '@/components/shared/page-header';
+import Image from 'next/image';
 
 export default function HivScreeningPage() {
   return (
@@ -8,6 +9,16 @@ export default function HivScreeningPage() {
         title="HIV Screening"
         description="This screening tool is confidential and designed to help you understand your potential risk and guide you towards appropriate resources."
       />
+      <div className="my-6 flex justify-center">
+        <Image 
+          src="https://placehold.co/600x400.png" 
+          alt="Cartoon of health worker performing a screening" 
+          width={600} 
+          height={400}
+          className="rounded-lg shadow-md"
+          data-ai-hint="health screening cartoon" 
+        />
+      </div>
       <ScreeningForm />
     </div>
   );
