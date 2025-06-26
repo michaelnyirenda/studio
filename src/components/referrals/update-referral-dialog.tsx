ab3c86151e0af80a5b41368af54786e8825d3dd6
Michael Nyirenda <michael00nyirenda@gmail.com>
2025-06-26 07:45:50 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Controller contro

--- src/components/referrals/update-referral-dialog.tsx
+++ src/components/referrals/update-referral-dialog.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import type { MockReferral } from '@/lib/mock-data';
@@ -232,22 +233,20 @@ export default function UpdateReferralDialog({ referral }: UpdateReferralDialogP
                       <FormLabel>Appointment Date & Time</FormLabel>
                       <Popover>
                         <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              variant={"outline"}
-                              className={cn(
-                                "w-full pl-3 text-left font-normal",
-                                !field.value && "text-muted-foreground"
-                              )}
-                            >
-                              {field.value ? (
-                                format(field.value, "PPP 'at' p")
-                              ) : (
-                                <span>Pick a date and time</span>
-                              )}
-                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                          </FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-full pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, "PPP 'at' p")
+                            ) : (
+                              <span>Pick a date and time</span>
+                            )}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
                         </PopoverTrigger>
                         <PopoverContent className="w-auto p-0" align="start">
                           <Calendar
