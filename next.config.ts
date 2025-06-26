c802297e1f675a0d9b79abf68e3145a2fe7b2626
Michael Nyirenda <michael00nyirenda@gmail.com>
2025-06-26 11:38:21 +0000

    Error: Invalid src prop (https://www.rollingstone.com/music/music-featur

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.rollingstone.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
