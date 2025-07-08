01ec929d2abda847ac431259ec383e6efc1ea4dd
Michael Nyirenda <michael00nyirenda@gmail.com>
2025-07-08 17:47:11 +0000

    make the nav bar a bit taller

--- src/components/admin/admin-navbar.tsx
+++ src/components/admin/admin-navbar.tsx
@@ -40,7 +40,7 @@ export default function AdminNavbar() {
 
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container flex h-16 items-center">
+            <div className="container flex h-20 items-center">
                 
                 {/* Left Aligned Items */}
                 <div className="flex flex-1 justify-start">
