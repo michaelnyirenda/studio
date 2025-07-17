814b79b3cb4f9fea1822827f0a04f69174da24ea
Michael Nyirenda <michael00nyirenda@gmail.com>
2025-07-17 09:46:21 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="fixed

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,7 +1,7 @@
 // src/components/shared/footer.tsx
 export default function Footer() {
   return (
-    <div className="fixed bottom-20 left-0 right-0 z-10 h-8 flex items-center justify-center bg-background/90 backdrop-blur-lg">
+    <div className="fixed bottom-0 left-0 right-0 z-0 h-8 flex items-center justify-center bg-background/90 backdrop-blur-lg">
       <p className="text-sm font-semibold text-foreground/80">
         Supported by OreSolve Consulting & Technology (Pty) Ltd
       </p>
